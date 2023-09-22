b425c17c69e44e4fb797e822a6f1c8671137be00
zorkpt <hugopocas@gmail.com>
2023-09-22 20:42:00 +0100

    Exercicio Padding

--- aulas/Program.cs
+++ aulas/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Maria".PadRight(20) + "Barcelos".PadRight(20));
+Console.WriteLine("Joao".PadRight(20) + "Braga".PadRight(20));
+Console.WriteLine("Pedro".PadRight(20) + "Porto".PadRight(20));
+
+
+Console.WriteLine("Maria".PadLeft(20) + "Barcelos".PadLeft(20));
+Console.WriteLine("Joao".PadLeft(20) + "Braga".PadLeft(20));
+Console.WriteLine("Pedro".PadLeft(20) + "Porto".PadLeft(20));
+
+
